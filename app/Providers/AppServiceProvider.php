b8581598a7de7337968528f4f896d515f782bb75
Iqbalrez <iqbalrr02@gmail.com>
2025-03-31 15:51:01 +0700

    fix app url in vercel env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -43,7 +43,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if(env('APP_ENV') == 'local'){
+        if(env('APP_ENV') == 'production'){
             URL::forceScheme('https');
         }
     }
