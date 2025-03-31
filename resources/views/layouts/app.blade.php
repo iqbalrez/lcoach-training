142b13ce7b791fbb74734f0eb38bc5c2bee91809
Iqbalrez <iqbalrr02@gmail.com>
2025-03-31 18:42:25 +0700

    fix css and js for vercel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,8 +21,6 @@
         <!-- Sweet Alert 2 -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.27/dist/sweetalert2.min.css">
 
-        <link rel="stylesheet" href="{{ asset('resources/css/app.css') }}">
-
 
     </head>
     <body class="font-[figtree] antialiased">
@@ -45,7 +43,6 @@
 
             <x-footer />
             
-            <script src="{{ asset('resources/js/app.js') }}"></script>
             <!-- Jquery -->
             <script src="https://code.jquery.com/jquery-3.7.0.min.js"
             integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
